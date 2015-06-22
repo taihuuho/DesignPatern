c1cdf6e18682f554dc581be3c458da6be3cab402
Tai Ho <hohuutai.ryan@gmail.com>
2015-06-22 15:12:35 -0500

    Singleton

---
+++ 1. Singleton/Singleton/src/singleton/Singleton.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package singleton;
+
+/**
+ *
+ * @author 984571
+ */
+public class Singleton {
+    
+    
+    private static class InnerClass{
+        static Singleton sharedInstance = new Singleton();
+    }
+    /**
+     * 
+     * @return  private sharedInstance instantiated
+     */
+    public static Singleton sharedInstance(){
+        return InnerClass.sharedInstance;
+    }
+    
+    /**
+     * Make the Constructor private, prevent instantiation from outside
+     */
+    private Singleton(){
+    
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
