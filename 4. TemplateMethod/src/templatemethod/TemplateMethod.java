839e1da8ccb7855a3f5c9d802d35b900f2126aa6
Tai Ho <hohuutai.ryan@gmail.com>
2015-06-25 10:13:29 -0500

    Template method

--- 4. TemplateMethod/src/templatemethod/TemplateMethod.java
+++ 4. TemplateMethod/src/templatemethod/TemplateMethod.java
@@ -16,6 +16,7 @@ public class TemplateMethod {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
